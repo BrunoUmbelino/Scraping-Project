e4eda8479eabd79b11b1e8eb01181e796b1752c8
Bruno Umbelino <bruno.umbelino0@gmail.com>
2023-01-02 01:33:49 -0400

    Criando Job teste

---
+++ Product Scraping/Jobs/ScrapingJob.cs
@@ -0,0 +1,14 @@
+﻿using Quartz;
+using System.Diagnostics;
+
+namespace Product_Scraping.Jobs
+{
+    public class ScrapingJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Debug.WriteLine("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            return Task.FromResult(true);
+        }
+    }
+}
--- Product Scraping/Program.cs
+++ Product Scraping/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using Product_Scraping.Jobs;
 using Product_Scraping.Models.Database;
 using Product_Scraping.Services;
+using Quartz;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +18,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddQuartz(q =>
+{
+    q.UseMicrosoftDependencyInjectionScopedJobFactory();
+    var jobKey = new JobKey("DemoJob");
+    q.AddJob<ScrapingJob>(opts => opts.WithIdentity(jobKey));
+
+    q.AddTrigger(opts => opts
+        .ForJob(jobKey)
+        .WithIdentity("DemoJob-trigger")
+        .WithCronSchedule("0/5 * * * * ?"));
+
+});
+
+builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
