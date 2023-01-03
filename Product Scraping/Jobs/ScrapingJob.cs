d455dab5511a351e30722129fe61aa3e20a1d142
Bruno Umbelino <bruno.umbelino0@gmail.com>
2023-01-02 20:14:44 -0400

    Add automated Scraping Job

--- Product Scraping/Jobs/ScrapingJob.cs
+++ Product Scraping/Jobs/ScrapingJob.cs
@@ -1,14 +1,28 @@
-﻿using Quartz;
+﻿using Microsoft.OpenApi.Any;
+using Product_Scraping.Models;
+using Product_Scraping.Services;
+using Quartz;
 using System.Diagnostics;
 
 namespace Product_Scraping.Jobs
 {
     public class ScrapingJob : IJob
     {
-        public Task Execute(IJobExecutionContext context)
+        private readonly ScrapingService _scrapingService;
+        private readonly ProductsService _productsService;
+
+        public ScrapingJob(ScrapingService scrapingService, ProductsService productsService)
         {
-            Debug.WriteLine("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-            return Task.FromResult(true);
+            _scrapingService = scrapingService;
+            _productsService = productsService;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Debug.WriteLine("Running Job.");
+            var produtos = _scrapingService.FreeFoodFactsScraping();
+            await _productsService.CreateManyAsync(produtos);
+            await Task.CompletedTask;
         }
     }
 }
