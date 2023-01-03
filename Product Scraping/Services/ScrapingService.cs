77c313d308d35fa5e064de43fc545f0d27251283
Bruno Umbelino <bruno.umbelino0@gmail.com>
2023-01-03 15:12:05 -0400

    refatoring ScrapingService

--- Product Scraping/Services/ScrapingService.cs
+++ Product Scraping/Services/ScrapingService.cs
@@ -4,70 +4,62 @@ using WebDriverManager;
 using OpenQA.Selenium;
 using Product_Scraping.Models.Database;
 using Product_Scraping.Models;
+using ZstdSharp.Unsafe;
 
 namespace Product_Scraping.Services
 {
     public class ScrapingService
     {
-        private IWebDriver _webDriver;
-        public ScrapingService()
+        private IWebDriver ?_webDriver;
+        public ScrapingService() => new DriverManager().SetUpDriver(new ChromeConfig());
+
+        public List<Product> FreeFoodFactsScraping()
         {
-            new DriverManager().SetUpDriver(new ChromeConfig());
             _webDriver = new ChromeDriver();
             _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
-        }
 
-        public async Task<List<Product>> FreeFoodFactsScraping()
-        {
             var openFoodFactsUrl = "https://world.openfoodfacts.org/";
             _webDriver.Navigate().GoToUrl(openFoodFactsUrl);
 
-            var items = _webDriver.FindElements(By.ClassName("list_product_a"));
-            var itemsLimitados = items.ToList().Take(3);
+            var productElementsList = _webDriver.FindElements(By.ClassName("list_product_a"));
+            var limitedListProductElements = productElementsList.ToList().Take(100);
 
-            var urlsDeProduto = new List<string>();
-            var produtos = new List<Product>();
-
-            foreach (var item in itemsLimitados)
+            var ProductUrls = new List<string>();
+            foreach (var productEl in limitedListProductElements)
             {
-                urlsDeProduto.Add(item.GetAttribute("href"));
+                ProductUrls.Add(productEl.GetAttribute("href"));
             }
 
-            foreach (var url in urlsDeProduto)
+            var products = new List<Product>();
+            foreach (var url in ProductUrls)
             {
-                var produto = new Product();
                 _webDriver.Navigate().GoToUrl(url);
 
-                var elCode = _webDriver.FindElements(By.Id("barcode")).FirstOrDefault()?.Text;
-                var elBarcode = _webDriver.FindElements(By.Id("barcode_paragraph")).FirstOrDefault()?.Text;
-                var elNomeDoProduto = _webDriver.FindElements(By.ClassName("title-1")).FirstOrDefault()?.Text;
-                var elQuantidade = _webDriver.FindElements(By.Id("field_quantity_value")).FirstOrDefault()?.Text;
-                var elCategorias = _webDriver.FindElements(By.Id("field_categories_value")).FirstOrDefault()?.Text;
-                var elEmbalagem = _webDriver.FindElements(By.Id("field_packaging_value")).FirstOrDefault()?.Text;
-                var elMarcas = _webDriver.FindElements(By.Id("field_brands_value")).FirstOrDefault()?.Text;
-                var elUrlDaImagem = _webDriver.FindElements(By.XPath("//img[@id='og_image']")).FirstOrDefault();
+                var code = _webDriver.FindElements(By.Id("barcode")).FirstOrDefault()?.Text;
+                var barcode = _webDriver.FindElements(By.Id("barcode_paragraph")).FirstOrDefault()?.Text;
+                var productName = _webDriver.FindElements(By.ClassName("title-1")).FirstOrDefault()?.Text;
+                var quantity = _webDriver.FindElements(By.Id("field_quantity_value")).FirstOrDefault()?.Text;
+                var categories = _webDriver.FindElements(By.Id("field_categories_value")).FirstOrDefault()?.Text;
+                var packaging = _webDriver.FindElements(By.Id("field_packaging_value")).FirstOrDefault()?.Text;
+                var brands = _webDriver.FindElements(By.Id("field_brands_value")).FirstOrDefault()?.Text;
+                var imageUrl = _webDriver.FindElements(By.XPath("//img[@id='og_image']")).FirstOrDefault();
 
-                var UrlDaImagem = elUrlDaImagem != null ? elUrlDaImagem.GetAttribute("src") : "";
+                var product = new Product();
+                if(code != null) product.Code = Convert.ToInt64(code);
+                if(barcode != null) product.BarCode = barcode.Replace("Barcode: ", "");
+                if (productName != null) product.ProductName = productName; 
+                product.Url = url;
+                if (quantity != null) product.Quantity = quantity;
+                if (categories != null) product.Categories = categories;
+                if (packaging != null) product.Packaging = packaging;
+                if (brands != null) product.Brands = brands;
+                if (imageUrl != null) product.ImageUrl = imageUrl.GetAttribute("src");
 
-                produto.Code = Convert.ToInt64(elCode);
-                produto.BarCode = elBarcode.Replace("Barcode: ", "");
-                produto.ProductName = elNomeDoProduto;
-                produto.Status = Status.Draft;
-                produto.ImportedT = DateTime.Now;
-                produto.Url = url;
-                produto.Quantity = elQuantidade;
-                produto.Categories = elCategorias;
-                produto.Packaging = elEmbalagem;
-                produto.Brands = elMarcas;
-                produto.ImageUrl = UrlDaImagem;
-
-                produtos.Add(produto);
-
+                products.Add(product);
             }
 
-            _webDriver.Quit();
-
-            return produtos;
+            _webDriver.Close();
+            return products;
         }
     }
 }
