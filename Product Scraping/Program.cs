0c13cdff81bb851cddea431ccf47f3b75315a1c3
Bruno Umbelino <bruno.umbelino0@gmail.com>
2023-01-02 20:15:52 -0400

    adjusting job configuration

--- Product Scraping/Program.cs
+++ Product Scraping/Program.cs
@@ -5,48 +5,53 @@ using Product_Scraping.Models.Database;
 using Product_Scraping.Services;
 using Quartz;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.Configure<FreeFoodFactsScrapingDatabaseSettings>(
-    builder.Configuration.GetSection(nameof(FreeFoodFactsScrapingDatabaseSettings)));
-
-builder.Services.AddSingleton<ProductsService>();
-builder.Services.AddSingleton<ScrapingService>();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddQuartz(q =>
+internal class Program
 {
-    q.UseMicrosoftDependencyInjectionScopedJobFactory();
-    var jobKey = new JobKey("DemoJob");
-    q.AddJob<ScrapingJob>(opts => opts.WithIdentity(jobKey));
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-    q.AddTrigger(opts => opts
-        .ForJob(jobKey)
-        .WithIdentity("DemoJob-trigger")
-        .WithCronSchedule("0/5 * * * * ?"));
+        builder.Services.Configure<FreeFoodFactsScrapingDatabaseSettings>(
+            builder.Configuration.GetSection(nameof(FreeFoodFactsScrapingDatabaseSettings)));
 
-});
+        builder.Services.AddSingleton<ProductsService>();
+        builder.Services.AddSingleton<ScrapingService>();
 
-builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+        builder.Services.AddQuartz(q =>
+        {
+            q.UseMicrosoftDependencyInjectionJobFactory();
+            var jobKey = new JobKey("ScrapingJob");
+            q.AddJob<ScrapingJob>(opts => opts.WithIdentity(jobKey));
+
+            q.AddTrigger(opts => opts
+                .ForJob(jobKey)
+                .WithIdentity("ScrapingJob-trigger")
+                .WithCronSchedule("0 * * ? * *")); /// Cron expression 0 0 2 ? * * *
+        });
+
+        builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+        var app = builder.Build();
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
